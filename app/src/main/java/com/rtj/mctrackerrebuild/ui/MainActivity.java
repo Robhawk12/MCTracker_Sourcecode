9eab52b69048605c4b8115ae318099710ceb1a69
Robert Johnson <rjo1275@wgu.edu>
2024-11-27 11:44:13 -0800

    Simple Sign in

--- app/src/main/java/com/rtj/mctrackerrebuild/ui/MainActivity.java
+++ app/src/main/java/com/rtj/mctrackerrebuild/ui/MainActivity.java
@@ -6,31 +6,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.rtj.mctrackerrebuild.R;
+import com.rtj.mctrackerrebuild.data.Repository;
+import com.rtj.mctrackerrebuild.data.UserRepository;
+import com.rtj.mctrackerrebuild.entities.User;
+import com.rtj.mctrackerrebuild.viewModel.UserViewModel;
 
 public class MainActivity extends AppCompatActivity {
+        EditText editUsername;
+        EditText editPassword;
+        String n = "Admin";
+        String p = "Password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //UserRepository repository = new UserRepository(getApplication());
+        //User user = new User();
+        editUsername = findViewById(R.id.editTextUsername);
+        editPassword = findViewById(R.id.editTextPassword);
+
         Button button = findViewById(R.id.loginButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String name = editUsername.getText().toString();
+                String password = editPassword.getText().toString();
                 Intent intent = new Intent(MainActivity.this,ClientListActivity.class);
-                startActivity(intent);
-            }
-        });
-        button = findViewById(R.id.newUserButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ClientListActivity.class);
-                startActivity(intent);
-            }
+               //user = repository.getUserByUserame(name);
+               if(name.equals("Admin") && password.equals("Password")) {
+                   startActivity(intent);
+                   Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+               }else {
+                   Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+               }
+           }
         });
     }
 }
